56eed82683b35d4c5e7d67c2c2e00f9d148ca565
Fábio Zunta <nonameeesg@gmail.com>
2022-05-24 20:47:46 -0300

    working on app js and intro to ejs

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(rqe, res){
-    res.send("server is running on port 3000...");
-});
+app.set("view engine", 'ejs');
+
+app.get("/", function(req, res){
+    let today = new Date();
+    let currentDay = today.getDay();
+    let day = "";
+
+    if (currentDay === 6 || currentDay === 0){
+        day = "weekend";
+    } else {
+        day = "weekday";
+        }
+
+    res.render("list", {kindOfDay: day});
+
+    // the challenger here now is to make it so the website displays the current day of the week
+    // at 14:04 lesson 266.
+
+    });
 
 app.listen(3000, function(){
     console.log("server is running on port 3000...");
