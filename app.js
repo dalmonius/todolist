d2f39599a2a7c804c0cff488a93dc9d39a1a1618
Fábio Zunta <nonameeesg@gmail.com>
2022-05-25 12:45:09 -0300

    displaying the weekdays, done

--- app.js
+++ app.js
@@ -10,31 +10,33 @@ app.get("/", function(req, res){
     let currentDay = today.getDay();
     let day = "";
 
-    if (currentDay === 0){
-        day = "sunday"
-    } else if (currentDay === 1){
-        day = "monday"
-    } else if (currentDay === 2){
-        day = "tuesday"
-    } else if (currentDay === 3){
-        day = "wednesday"
-    } else if (currentDay === 4){
-        day = "thursday"
-    } else if (currentDay === 5){
-        day = "friday"
-    } else if (currentDay === 6){
-        day = "saturday"
-    } else {
-        day = "not a weekday"
+    switch(currentDay){
+        case 0:
+            day = "sunday"
+            break;
+        case 1:
+            day = "monday"
+            break;
+        case 2:
+            day = "tuesday"
+            break;
+        case 3:
+            day = "wednesday"
+            break;
+        case 4:
+            day = "thursday"
+            break;
+        case 5:
+            day = "friday"
+            break;
+        case 6:
+            day = "saturday"
+            break;
+        default:
+            day = "day not fetched"        
     }
-
-    // I know this is not best practice, probably it's a better idea to use a switch statement
-
+    
     res.render("list", {kindOfDay: day});
-
-    // the challenger here now is to make it so the website displays the current day of the week
-    // at 14:04 lesson 266.
-
     });
 
 app.listen(3000, function(){
