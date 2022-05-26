24bcefd441974563038f881059938a878cb4625b
Fábio Zunta <nonameeesg@gmail.com>
2022-05-26 10:16:44 -0300

    working on app.js, still learning ejs

--- app.js
+++ app.js
@@ -2,42 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+let newTasks = [];
 
 app.set("view engine", 'ejs');
 
 app.get("/", function(req, res){
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
+    let options = {weekday: 'long', month: 'long', day: 'numeric'};
 
-    switch(currentDay){
-        case 0:
-            day = "sunday"
-            break;
-        case 1:
-            day = "monday"
-            break;
-        case 2:
-            day = "tuesday"
-            break;
-        case 3:
-            day = "wednesday"
-            break;
-        case 4:
-            day = "thursday"
-            break;
-        case 5:
-            day = "friday"
-            break;
-        case 6:
-            day = "saturday"
-            break;
-        default:
-            day = "day not fetched"        
-    }
-    
-    res.render("list", {kindOfDay: day});
+    res.render("list", {kindOfDay: today.toLocaleDateString("en-US", options), newListItem: newTasks});
     });
+
+app.post("/", function(req, res){
+    let newTask = req.body.newItem
+    newTasks.push(newTask);
+    res.redirect("/");
+});
 
 app.listen(3000, function(){
     console.log("server is running on port 3000...");
