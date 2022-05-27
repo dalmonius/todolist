b8d675b59553e96c0268baf169d12139682a402d
Fábio Zunta <nonameeesg@gmail.com>
2022-05-27 14:18:57 -0300

    app.post at /work not working

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let newTasks = ["demo task", "demo task"];
+let newTasks = [];
+let workTasks = [];
 
 app.set("view engine", 'ejs');
 
@@ -13,13 +14,29 @@ app.get("/", function(req, res){
     let today = new Date();
     let options = {weekday: 'long', month: 'long', day: 'numeric'};
 
-    res.render("list", {kindOfDay: today.toLocaleDateString("en-US", options), newListItems: newTasks});
+    res.render("list", {listTitle: today.toLocaleDateString("en-US", options), newListItems: newTasks});
     });
 
 app.post("/", function(req, res){
-    let newTask = req.body.newItem
+    let newTask = req.body.newItem;
     newTasks.push(newTask);
     res.redirect("/");
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work", newListItems: workTasks});
+});
+
+app.post("/work", function(req, res){
+    console.log(req.body.newItem);  
+    let item = req.body.newItem;
+    if (req.body.newItem === "work"){
+        workTasks.push(item);
+        res.redirect("/work");
+    } else {
+        newListItems.push(item);
+        res.redirect("/");
+    }
 });
 
 app.listen(3000, function(){
